ed871a809ece87a4d7247f7a645fd4a6d935b5a0
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-08 22:41:42 -0400

    feat: Add Home, Coin, and WatchList pages to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Layout } from './pages/Layout'
+import { Home } from './pages/Home'
+import { Coin } from './pages/Coin'
+import { WatchList } from './pages/WatchList'
 
 function App() {
 
@@ -10,9 +13,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Layout />} >
-            <Route></Route>
-            <Route></Route>
-            <Route></Route>
+            <Route index element={<Home />}></Route>
+            <Route path="watchlist" element={<WatchList />}></Route>
+            <Route path="coin" element={<Coin />}></Route>
           </Route>
         </Routes>
       </BrowserRouter>
