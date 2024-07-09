25f5aa948274282b6faed271ac55fc5e13920b2c
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-08 22:18:30 -0400

    feat: Add routing to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
     <>
+
+      <BrowserRouter>
+        <Routes>
+          <Route>
+            <Route></Route>
+            <Route></Route>
+            <Route></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
 
     </>
   )
