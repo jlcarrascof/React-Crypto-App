652e5c01004f57c556441e7838b2225e7fe402b8
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-08 22:53:05 -0400

    feat: Update import statements in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import { Layout } from './pages/Layout'
-import { Home } from './pages/Home'
-import { Coin } from './pages/Coin'
-import { WatchList } from './pages/WatchList'
-import { NoPage } from './pages/NoPage'
+import Layout from './pages/Layout'
+import Home from './pages/Home'
+import Coin from './pages/Coin'
+import WatchList from './pages/WatchList'
+import NoPage from './pages/NoPage'
 
 function App() {
 
@@ -13,7 +13,7 @@ function App() {
 
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Layout />} >
+          <Route path='/' element={<Layout />}>
             <Route index element={<Home />}></Route>
             <Route path="watchlist" element={<WatchList />}></Route>
             <Route path="coin" element={<Coin />}></Route>
