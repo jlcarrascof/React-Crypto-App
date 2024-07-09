e5c8a7acf93c682628b0699733c27130629c86ba
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-08 22:59:01 -0400

    feat: Add navigation bar to Layout component

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Outlet, Link } from 'react-router-dom';
 
 const Layout = () => {
   return (
    <div>
      <h1>CRYPTO APP</h1>
+     <nav>
+       <ul>
+         <li></li>
+         <li></li>
+       </ul>
+     </nav>
+     <Outlet></Outlet>
    </div>
   )
 }
